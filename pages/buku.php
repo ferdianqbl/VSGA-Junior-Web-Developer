e105b4bdc4d5279a9bdada18fda720edab3b398e
Alwi Ghozali <38102286+Muhal123@users.noreply.github.com>
2021-08-26 19:09:13 +0700

    ngilangin search

--- pages/buku.php
+++ pages/buku.php
@@ -5,10 +5,9 @@ include'Koneksi.php';
 <div class="container-fluid">
     <div class="card mb-3">
         <div class="card-header">
-            <i class="fas fa-table"></i> Data buku
-            <a href="index.php?p=buku-input" class="btn btn-sm btn-info">Tambah buku</a>
+            <i class="fas fa-table"></i> Data Buku
+            <a href="index.php?p=buku-input" class="btn btn-sm btn-info">Tambah Data</a>
         </div>
-    </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -22,30 +21,7 @@ include'Koneksi.php';
                 </thead>
                 <tbody>
                     
-                <?php 
-                    $batas = 5;
-                    extract($_GET);
-                    if(empty($hal)){
-                        $posisi = 0;
-                        $hal = 1;
-                        $nomor = 1;
-                    }else {
-                        $posisi = ($hal-1)*$batas;
-                        $nomor = $posisi+1;
-                    }
-
-                    if($_SERVER['REQUEST_METHOD']== "POST"){
-                        $pencarian = trim(mysqli_real_escape_string($db, $_POST['pencarian']));
-                        if($pencarian != ""){
-                            $sql = "SELECT * FROM t_buku WHERE nama LIKE '%pencarian%'OR id_buku LIKE '%pencarian%' OR jk LIKE '%pencarian%' OR alamat LIKE '%pencarian%'";
-                            $query = $sql;
-                            $queryJml = $sql;
-                        }else{
-                            $query = "SELECT * FROM t_buku LIMIT $posisi, $batas";
-                            $queryJml = "SELECT * FROM t_buku";
-                            $no = $posisi * 1;
-                        }
-                    }   
+                <?php  
                     $query = mysqli_query($db, "SELECT * FROM t_buku");
                     $data = mysqli_fetch_array($query);
 
