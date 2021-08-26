8a2beb21ff1d3ac7e6f51d77415088356fa464e5
Alwi Ghozali <38102286+Muhal123@users.noreply.github.com>
2021-08-26 19:08:02 +0700

    Update anggota.php
    Nambahin Card Header xD

--- pages/anggota.php
+++ pages/anggota.php
@@ -5,21 +5,10 @@ include'Koneksi.php';
 <div class="container-fluid">
     <div class="card mb-3">
         <div class="card-header">
-            <div class="row justify-content-between">
-                <div class="col-4">
-                    <i class="fas fa-table"></i> Data Anggota
-                    <a href="index.php?p=anggota-input" class="btn btn-sm btn-info">Tambah Anggota</a>
-                    <a target="_blank" href="pages/cetak.php" class="btn btn-lg btn-primary bx bx-printer"></a>
-                </div>
-                <div class="col-4">
-                    <form class="d-flex" method="POST">
-                        <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" name="pencarian">
-                        <button class="btn btn-primary" type="submit" name="search" value="search" > Search</button>
-                    </form>
-                </div>
-            </div>
+            <i class="fas fa-table"></i> Data Anggota
+            <a href="index.php?p=anggota-input" class="btn btn-sm btn-info">Tambah Data</a>
+            <a target="_blank" href="pages/cetak.php" class="btn btn-lg btn-primary bx bx-printer"></a>
         </div>
-    </div>
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -37,29 +26,6 @@ include'Koneksi.php';
                 <tbody>
                     
                 <?php 
-                    $batas = 5;
-                    extract($_GET);
-                    if(empty($hal)){
-                        $posisi = 0;
-                        $hal = 1;
-                        $nomor = 1;
-                    }else {
-                        $posisi = ($hal-1)*$batas;
-                        $nomor = $posisi+1;
-                    }
-
-                    if($_SERVER['REQUEST_METHOD']== "POST"){
-                        $pencarian = trim(mysqli_real_escape_string($db, $_POST['pencarian']));
-                        if($pencarian != ""){
-                            $sql = "SELECT * FROM t_anggota WHERE nama LIKE '%pencarian%' OR id_anggota LIKE '%pencarian%' OR jk LIKE '%pencarian%' OR alamat LIKE '%pencarian%'";
-                            $query = $sql;
-                            $queryJml = $sql;
-                        }else{
-                            $query = "SELECT * FROM t_anggota LIMIT $posisi, $batas";
-                            $queryJml = "SELECT * FROM t_anggota";
-                            $no = $posisi * 1;
-                        }
-                    }   
                     $query = mysqli_query($db, "SELECT * FROM t_anggota");
                     $data = mysqli_fetch_array($query);
 
