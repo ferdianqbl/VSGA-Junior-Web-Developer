90b002c9505472e1668023db2190d73494f80c2b
Ardia Refdinal <69832819+reffardieza11@users.noreply.github.com>
2021-08-26 14:30:21 +0700

    Add files via upload

---
+++ proses/peminjaman-input-proses.php
@@ -0,0 +1,24 @@
+<?php
+
+include'../Koneksi.php';
+
+
+if(isset($_POST['simpan'])){
+extract($_POST);
+
+$anggota = $_POST['anggota'];
+$buku    = $_POST['buku'];
+$tgl     = date("Y-m-d");
+
+$query	= mysqli_query ($db, "INSERT INTO t_transaksi (id_transaksi, id_anggota, id_buku, tgl_pinjam, tgl_kembali) value (' ','$anggota','$buku','$tgl','')");
+
+if($query){
+    echo "<script>alert('Berhasil Menambahkan Data');window.location.href='../index.php?p=transaksi-peminjaman';</script>";
+}else{
+	echo "<script type='text/javascript'>
+	onload =function(){
+		alert('Gagal Menambah Data');
+	}
+</script>";
+}
+}
